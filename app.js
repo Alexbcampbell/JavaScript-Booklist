852b876ea82ec4a2c9d59687f70793e51c3746ff
alex Campbell <alexbe.campbell@gmail.com>
2022-05-27 00:40:57 -0500

    added event listeners, constructor functions

---
+++ app.js
@@ -0,0 +1,54 @@
+//Book constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+
+//UI constructor
+function UI(){}
+
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+
+    //create tr element
+    const row = document.createElement('tr');
+
+    //insert calls
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href = "#" class="delete">X</a></td>
+    `
+
+    list.appendChild(row);
+}
+
+//clear fields
+UI.prototype.clearFields = function(){
+    document.getElementById
+}
+
+//event listeners
+document.getElementById('book-form').addEventListener('submit',
+    function(e){
+    //get form values
+    const title = document.getElementById('title').value,
+            author = document.getElementById('author').value,
+            isbn = document.getElementById('isbn').value
+    //instantiating book
+    const book = new Book(title, author, isbn);
+
+    //instantiate ui object
+    const ui = new UI();
+
+    //add book to list
+    ui.addBookToList(book);
+
+    //Clear fields
+    ui.clearFields();
+
+    e.preventDefault();
+})
